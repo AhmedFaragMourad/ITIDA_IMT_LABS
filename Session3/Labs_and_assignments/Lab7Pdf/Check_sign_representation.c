8a422351f0325d8d7d991a7e76f39f028dd141fb
AhmedFaragMourad <faraghussin67@yahoo.com>
2023-09-22 14:10:54 +0300

    Session3

---
+++ Session3/Labs_and_assignments/Lab7Pdf/Check_sign_representation.c
@@ -0,0 +1,9 @@
+#include"stdio.h"
+
+
+int main(){
+	unsigned int x=-1;
+	printf("%u",x);
+	
+	return 0;
+}
---
+++ Session3/Labs_and_assignments/Lab7Pdf/sizeof.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+
+
+int main(){
+
+	printf("size of char = %d\n",sizeof(char));
+	printf("size of short int = %d\n",sizeof(short int));
+	printf("size of long int = %d\n",sizeof(long int));
+	printf("size of defualt int = %d\n",sizeof(int));
+	printf("size of float = %d\n",sizeof(float));
+	printf("size of double = %d\n",sizeof(double));
+	
+	return 0;
+}
